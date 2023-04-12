6d37ba96be5ac64922df8c120dc2ae339d229788
Ellie Boyd <86267168+ellieragan@users.noreply.github.com>
2023-04-12 13:11:56 -0400

    post test build

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+
+function count() {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+}
+
+setInterval(count, 1000);
